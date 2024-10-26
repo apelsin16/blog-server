4af5ca4cfab252e52cdfd11241814d6335a29967
apelsin16 <apelsin@i.ua>
2024-10-26 20:09:45 +0200

    connect to DB fixed

--- services/connect.js
+++ services/connect.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 const config = require("../config/db");
 
 const url = config.uri;
@@ -7,22 +7,23 @@ const dbName = config.dbName;
 let db;
 
 const connectToDb = async () => {
-    const client = new MongoClient(url);
-    try {
-        await client.connect();
-        db = client.db(dbName);
-        console.log('Connected to MongoDB');
-    } catch {
-        console.error('Failed to connect to database:', error);
-        throw error;
-    }
+  const client = new MongoClient(url);
+  try {
+    await client.connect();
+    db = client.db(dbName);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    // Передаємо error як параметр в catch
+    console.error("Failed to connect to database:", error);
+    throw error; // Якщо необхідно, знову генеруємо помилку для обробки на іншому рівні
+  }
 };
 
 const getDb = () => {
-    if (!db) {
-        throw new Error('Database not initialized');
-    }
-    return db;
+  if (!db) {
+    throw new Error("Database not initialized");
+  }
+  return db;
 };
 
 module.exports = { connectToDb, getDb };
